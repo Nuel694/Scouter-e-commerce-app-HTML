61e9e3066c511fddb73f6a269afbb3341aa211fe
Nuel Mene <nuelmene69@gmail.com>
2026-01-25 10:50:23 +0100

    adjusted and made the codes easier to read by rearranging them in a clean and  structured way. I also added and modified comments on each file for easy understanding and reference

--- script.js
+++ script.js
@@ -1,7 +1,54 @@
 
+/*This code is for toggling the menu links*/
+
 const menuToggle = document.querySelector(".menu-toggle");
+
+/* document → the entire web page
+
+querySelector() → finds one element in the page
+
+".menu-toggle" → a CSS selector (class) */
+
+
+
 const navLinks = document.querySelector(".nav-links");
+
+/*This is to select the hamburger button
+
+This is to select the navigation links.
+
+Same idea:
+
+Finds the <ul> that contains your menu links
+
+Stores it so we can change its behavior
+
+At this point:
+
+menuToggle → the ☰ button
+
+navLinks → the hidden menu list*/
+
+
 
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
+
+/*This is the code that runs when you click the hamburger button.
+
+addEventListener → waits for something to happen
+
+"click" → the event we care about
+
+() => {} → a function that runs when the click happens
+
+'HOW CSS AND JAVASCRIPT WORK TOGETHER HERE'
+eg.
+ .nav-links {
+  display: none;
+}
+
+.nav-links.active {
+  display: flex;
+}*/
