676f6499315b7001787ff5c15d01f6b8a609f4fc
Nuel Mene <nuelmene69@gmail.com>
2026-01-25 12:00:19 +0100

    added toggle functionality in javascript and more css code for nav menu and links

--- script.js
+++ script.js
@@ -2,53 +2,23 @@
 /*This code is for toggling the menu links*/
 
 const menuToggle = document.querySelector(".menu-toggle");
-
-/* document → the entire web page
-
-querySelector() → finds one element in the page
-
-".menu-toggle" → a CSS selector (class) */
-
-
-
 const navLinks = document.querySelector(".nav-links");
-
-/*This is to select the hamburger button
-
-This is to select the navigation links.
-
-Same idea:
-
-Finds the <ul> that contains your menu links
-
-Stores it so we can change its behavior
-
-At this point:
-
-menuToggle → the ☰ button
-
-navLinks → the hidden menu list*/
+const navItems = document.querySelectorAll(".nav-links a"); 
 
 
+/*This code is for toggling on and off menu link*/
 
 menuToggle.addEventListener("click", () => {
   navLinks.classList.toggle("active");
 });
 
-/*This is the code that runs when you click the hamburger button.
+/*This code is for when you click a link in the nav menu
+ and the toggle resets or closes  automatically*/
 
-addEventListener → waits for something to happen
+navItems.forEach(link => {
+  link.addEventListener("click", () => {
+     navItems.forEach(item => item.classList.remove("active"));
+  });
+});
 
-"click" → the event we care about
 
-() => {} → a function that runs when the click happens
-
-'HOW CSS AND JAVASCRIPT WORK TOGETHER HERE'
-eg.
- .nav-links {
-  display: none;
-}
-
-.nav-links.active {
-  display: flex;
-}*/
